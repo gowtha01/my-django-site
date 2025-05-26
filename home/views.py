1f4eb3ef61bdeccabd2e3a7cffa1a04743e071a7
gowtha01 <gowthamgowda07@gmail.com>
2025-05-26 12:22:29 +0530

    Add landing page templates in correct path

--- home/views.py
+++ home/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 
 def landing1(request):
     # Facebook Pixel can be added in the template
-    return render(request, "landing1.html")
+    return render(request, "home/landing1.html")
 
 def landing2(request):
-    return render(request, "landing2.html")
+    return render(request, "home/landing2.html")
 
 def home_view(request):
     return HttpResponse("<h1>Hello!</h1><p>New videos will be uploaded soon.</p>")
