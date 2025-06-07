d97488da8b0a279906521619fa326b13720244b4
gowtha01 <gowthamgowda07@gmail.com>
2025-06-07 20:51:24 +0530

    Use environment variables for SECRET_KEY and DEBUG in Django settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!%(@43p*wad--xz&+h^apb*b62lj!-7)^$0&niu^2ihbd5bf&9'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-!%(@43p*wad--xz&+h^apb*b62lj!-7)^$0&niu^2ihbd5bf&9')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['my-django-site-1.onrender.com']
 
